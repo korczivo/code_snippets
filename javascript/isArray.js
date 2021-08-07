62eada530572cdb6bd8522527133ed5854b6afa6
Łukasz Korcz <lukaszwp55@gmail.com>
2021-08-07 13:55:00 +0200

    isArray

---
+++ javascript/isArray.js
@@ -0,0 +1,13 @@
+// Useful helper function for verifying a given argument is an array 🚀 🚀 🚀
+
+function isArray(obj) {
+  return Object.prototype.toString.call(obj) === '[object Array]';
+}
+
+// Usage 💪 💪 💪
+
+console.log(isArray([])) // true ✅
+console.log(isArray({})) // false ❌
+console.log(isArray('string')) // false ❌
+console.log(isArray(['string'])) // true ✅
+console.log(isArray([{key: 'string'}])) // true ✅
