0bf6903e9b15ab1cb5b2c0588b5e17f309a4b0ea
Łukasz Korcz <lukaszwp55@gmail.com>
2021-08-30 19:02:04 +0200

    spread

---
+++ javascript/spred.js
@@ -0,0 +1,31 @@
+// Concatenating arrays and objects
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5];
+let newArray = [...arr1, ...arr2];
+console.log(newArray);
+// Output: [ 1, 2, 3, 4, 5 ]
+
+// Copying array elements
+let arr = ["a", "b", "c"];
+let newArray = [...arr];
+console.log(newArray);
+// Output: ["a", "b", "c"]
+
+// Expanding arrays
+let arr = ["a", "b"];
+let newArray = [...arr, "c", "d"];
+console.log(newArray);
+// Output: ["a", "b", "c", "d"]
+
+// Merging objects
+const userBasic = {
+  name: "Jen",
+  age: 22,
+};
+const userMoreInfo = {
+  country: "Argentina",
+  city: "Córdoba",
+};
+
+const user = {...userBasic, ...userMoreInfo};
+//Output: {  name: "Jen",  age: 22, country: "Argentina", city: "Córdoba" }
