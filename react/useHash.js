97defcde9d672c9f96ba344bfeb713e148257a15
Łukasz Korcz <lukaszwp55@gmail.com>
2021-10-03 19:48:53 +0200

    useHash react hook

---
+++ react/useHash.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+const useHash = (initialValue=null) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.location.hash
+      return item ? item.slice(1) : initialValue
+    } catch (error) {
+      console.log(error)
+      return initialValue
+    }
+  })
+  const setValue = value => {
+    try {
+      setStoredValue(value)
+      history.pushState(null, null, `#${value}`)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return [storedValue, setValue]
+}
