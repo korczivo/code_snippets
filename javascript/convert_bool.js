deabb7ca35510079abaaf5aff6180fd4572b6232
Łukasz Korcz <lukaszwp55@gmail.com>
2021-12-09 21:50:59 +0100

    convert boolean

---
+++ javascript/convert_bool.js
@@ -0,0 +1,9 @@
+const myVar = null;
+const mySecondVar = 1;
+
+console.log( Boolean(myVar) ) // false
+console.log( !!myVar ) // false
+
+
+console.log( Boolean(mySecondVar) ) // true
+console.log( !!mySecondVar ) // true
