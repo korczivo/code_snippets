2f468a226a6f1548d8a95040916855825a2d3b24
Łukasz Korcz <lukaszwp55@gmail.com>
2021-12-31 18:50:57 +0100

    URLSearchParams

---
+++ javascript/helpers/URLSearchParams.js
@@ -0,0 +1,9 @@
+// The URLSearchParams interface defines utility methods to work with the query string of a URL.
+
+const urlParams = new URLSearchParams("?post=1234&action=edit");
+
+console.log(urlParams.has('post')); // true
+console.log(urlParams.get('action')); // "edit"
+console.log(urlParams.getAll('action')); // ["edit"]
+console.log(urlParams.toString()); // "?post=1234&action=edit"
+console.log(urlParams.append('active', '1')); // "?post=1234&action=edit&active=1"
