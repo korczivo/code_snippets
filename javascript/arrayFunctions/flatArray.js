3e211e2723eace554088c9c4b32ad0e648e8d9e2
korczivo <lukaszwp55@gmail.com>
2023-03-06 19:19:48 +0100

    update

--- javascript/arrayFunctions/flatArray.js
+++ javascript/arrayFunctions/flatArray.js
@@ -10,3 +10,10 @@ console.log(flatArray([1, [1], 1, 1])) // [ 1, 1, 1, 1 ]
 console.log(flatArray([1, [1, [1, [1, 1], 1], 1], 1], 2)) // [ 1, 1, 1, [ 1, 1 ], 1, 1, 1 ]
 console.log(flatArray([1, [1, [1, [1, 1], 1], 1], 1], 3)) // [1, 1, 1, 1, 1, 1, 1, 1]
 
+const flatArrayInfinity = arr => arr.reduce((acc, curr) => (
+    acc.concat(Array.isArray(curr) ? flatArrayInfinity(curr) : curr)
+), [])
+
+console.log(flatArrayInfinity([false, 2, [3, [[[[]]]], true], [5, 6, [7, 8, 8]]])) // [false,2,3,true,5,6,7,8,8]
+console.log(flatArrayInfinity([1, 2, [3, 4], [5, 6, [7, 8, 8]]])) // [1,2,3,4,5,6,7,8,8]
+console.log(flatArrayInfinity([[], [[], []]])) // []
