c6a01ed73d936df5f4c6cfc29038698a79f13ffa
korczivo <lukaszwp55@gmail.com>
2023-03-08 08:54:35 +0100

    sum of digits / digital root

---
+++ kata6/sum-digits.js
@@ -0,0 +1,15 @@
+function digitalRoot(n) {
+    const arr = Array.from(String(n), (num) => Number(num));
+    return arr.reduce((acc, curr) => {
+        const sum = acc + curr;
+        if (sum >= 10) {
+            return digitalRoot(sum)
+        }
+        return sum;
+    }, 0)
+}
+
+console.log(digitalRoot(5)) // 5
+console.log(digitalRoot(16)) // 7
+console.log(digitalRoot(942)) // 6
+console.log(digitalRoot(132189)) // 6
